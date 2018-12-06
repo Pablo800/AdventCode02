6487d3c2891b7379dabf2571fb25642e92f51795
pablo800 <pabloan.990@gmail.com>
2018-12-06 18:39:39 +0100

    Day 2: Inventory Management System

--- prueba01.py
+++ prueba01.py
@@ -4,4 +4,31 @@ Created on 6 dic. 2018
 @author: Pablo
 '''
 
-print("Esto es una prueba")
+def group_letters(input):
+    patterns = {}
+    for letra in input:
+        patterns[letra] = patterns.get(letra, 0) + 1
+    return patterns
+
+
+def getRepetitions(input):
+    values = group_letters(input)
+    return(2 in values.values(), 3 in values.values())
+        
+
+boxes = [s[:-1] for s in open('input.txt').readlines()]
+
+#repetitions: Tupla de dos
+# - [0] True: letra se repite dos veces
+# - [1] True: letra se repite tres veces
+repetitions = list(map(getRepetitions, boxes))
+
+boxes_2 = len(list(filter(lambda x: x[0] is True, repetitions)))
+boxes_3 = len(list(filter(lambda x: x[1] is True, repetitions)))
+
+print(boxes_2)
+print(boxes_3)
+
+print(boxes_2 * boxes_3)
+
+
